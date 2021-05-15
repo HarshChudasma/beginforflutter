b75b09976e1ad8c3751893d6bc26d5386120568b
Harsh <harshchudasma010@gmail.com>
2021-05-15 17:10:57 +0530

    day9 completed

---
+++ lib/models/catelog.dart
@@ -0,0 +1,13 @@
+class Item{
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.description, this.price, this.color, this.image);
+}
+final products = [
+  Item("flutter001", "Harsh", "Chudasma harsh", 100, "#33505a", "")
+];
